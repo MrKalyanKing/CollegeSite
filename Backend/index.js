62e325bdd04e4be03dc61255891ad4d901eeac22
MrKalyanNick <badhavathkalyan08@gmail.com>
2024-12-08 08:53:27 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ dotenv.config()
 import mongoose from 'mongoose'
 import router from './Router/router.js'
 import cors from 'cors'
-import gopd from 'gopd';
+import gopd from 'gOPD';
 
 const port=process.env.PORT|| 3000;
 const app=express()
